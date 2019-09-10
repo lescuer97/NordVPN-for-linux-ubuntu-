2ce5c909aada94439bdce6a97f57fa74cb02ab1a
leo <leo@pop-os.localdomain>
2019-09-10 23:38:30 +0200

    commit 3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-
 #downloads nord vpn file
+
 cd ~/Downloads/
 
 sudo wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
@@ -13,7 +13,7 @@ sudo apt install nordvpn
 
 cd ~
 
-sudo wget https://github.com/lescuer97/NordVPN-for-linux-ubuntu-/tree/master/vpn
+sudo wget -P https://github.com/lescuer97/NordVPN-for-linux-ubuntu-/tree/master/vpn
 
 cd vpn
 chmod +x canada.sh germany.sh hk.sh holand.sh singapore.sh switzerland.sh uk.sh us.sh
